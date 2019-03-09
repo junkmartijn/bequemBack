9ad3e77c8ab16b8f51343b8d6d613b9dcab22c1d
Martijn <Martijn@LAPTOPM>
2019-03-09 14:57:01 +0100

    added basic ntp functionality

--- ESP12E_webserver_playground.ino
+++ ESP12E_webserver_playground.ino
@@ -3,6 +3,7 @@
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
+#include <EthernetUdp.h>
 #include <FS.h>   // Include the SPIFFS library
 #include <ArduinoJson.h>
 #include "SettingsManager.h"
@@ -11,6 +12,20 @@
 SettingsManager settings_manager;
 ESP8266WiFiMulti wifi_multi;
 ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+EthernetUDP Udp;
+
+#define TimeZoneOffset -1 //-2 zomertijd / -1 wintertijd
+unsigned long t_raw;
+// local port to listen for UDP packets
+unsigned int localPort = 8888;
+
+// NTP time stamp is in the first 48 bytes of the message
+const int NTP_PACKET_SIZE = 48;
+
+//buffer to hold incoming and outgoing packets
+byte packetBuffer[NTP_PACKET_SIZE];
+
+IPAddress timeServer(192, 168, 1, 3);
 
 const int output_pin = 2;
 bool force_constant = LOW;
@@ -45,6 +60,9 @@ void setup() {
 	else {
 		Serial.println("Error setting up MDNS responder!");
 	}
+
+	setSyncProvider(getNtpTime);
+	setSyncInterval(10);
 
 	server.begin();
 	server.on("/api/time", HTTP_GET, WebApiTimeGet);
@@ -352,3 +370,72 @@ void WebApiNotFound() {
 }
 
 #pragma endregion webapi methods
+
+#pragma region NTP
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress & address)
+{
+	// set all bytes in the buffer to 0
+	memset(packetBuffer, 0, NTP_PACKET_SIZE);
+
+	// Initialize values needed to form NTP request
+	// (see URL above for details on the packets)
+	packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+	packetBuffer[1] = 0;     // Stratum, or type of clock
+	packetBuffer[2] = 6;     // Polling Interval
+	packetBuffer[3] = 0xEC;  // Peer Clock Precision
+
+	// 8 bytes of zero for Root Delay & Root Dispersion
+	packetBuffer[12] = 49;
+	packetBuffer[13] = 0x4E;
+	packetBuffer[14] = 49;
+	packetBuffer[15] = 52;
+
+	// all NTP fields have been given values, now
+	// you can send a packet requesting a timestamp:
+	Udp.beginPacket(address, 123); //NTP requests are to port 123
+	Udp.write(packetBuffer, NTP_PACKET_SIZE);
+	Udp.endPacket();
+}
+
+time_t getNtpTime()
+{
+	Serial.print("Syncing time... ");
+
+	// send an NTP packet to a time server
+	sendNTPpacket(timeServer);
+
+	// wait to see if a reply is available
+	Alarm.delay(1000);
+
+	if (Udp.parsePacket()) {
+		// We've received a packet, read the data from it
+		Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+		//the timestamp starts at byte 40 of the received packet and is four bytes,
+		// or two words, long. First, esxtract the two words:
+
+		unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+		unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+
+		// combine the four bytes (two words) into a long integer
+		// this is NTP time (seconds since Jan 1 1900):
+		unsigned long secsSince1900 = highWord << 16 | lowWord;
+
+		// now convert NTP time into everyday time:
+		// Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+		const unsigned long seventyYears = 2208988800UL;
+
+		// subtract seventy years:
+		t_raw = secsSince1900 - seventyYears;
+		t_raw -= TimeZoneOffset * 60 * 60;
+		Serial.println("succes!");
+		return t_raw;
+	}
+	Serial.println("failed!");
+	return 0;
+}
+
+
+#pragma endregion NTP
